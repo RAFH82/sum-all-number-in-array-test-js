eb15324aa15ca029eda2b369f02a6b04fb4d7506
RAFH82 <hrechkar@gmail.com>
2020-10-31 17:29:32 +0000

    finished exercise

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 function sumItems(array) {
-  // Sum all the numbers in the array
-}
+  sum = 0;
+  for (element of array) {
+    if (Array.isArray(element)) {
+     sum += sumItems(element);
+    } else {
+    sum += element;
+    }
+  }
+  return sum;
+};
 
 module.exports = sumItems;
